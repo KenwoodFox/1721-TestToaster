8f5b07d0942e30bb9c806c900cb3eac1d8e0759a
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2017-12-01 16:50:05 -0500

    Updated

--- src/org/usfirst/frc/team1721/robot/Robot.java
+++ src/org/usfirst/frc/team1721/robot/Robot.java
@@ -24,7 +24,7 @@ public class Robot extends SampleRobot {
 	RobotDrive myRobot = new RobotDrive(0, 1); // class that handles basic drive
 												// operations
 	Joystick leftStick = new Joystick(0); // set to ID 1 in DriverStation
-	Joystick rightStick = new Joystick(1); // set to ID 2 in DriverStation
+	// Joystick rightStick = new Joystick(1); // set to ID 2 in DriverStation
 
 	public Robot() {
 		myRobot.setExpiration(0.1);
@@ -37,7 +37,7 @@ public class Robot extends SampleRobot {
 	public void operatorControl() {
 		myRobot.setSafetyEnabled(true);
 		while (isOperatorControl() && isEnabled()) {
-			myRobot.tankDrive(leftStick, rightStick);
+			myRobot.tankDrive(leftStick, leftStick);
 			Timer.delay(0.005); // wait for a motor update time
 		}
 	}
